427fa7569cdf549b5a4ce6726b45ccd71c486e4d
tangmengjin <tangmengjin@foxmail.com>
2023-11-16 14:25:26 +0800

    调整样式和TEST环境一致

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,75 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script >
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div>
+    <div class="middle-box text-center loginscreen  animated fadeInDown">
+      <div>
+        <div style="padding-top: 180px;">
+          <img src="src/static/Content/css/Images/logo.png" />
+        </div>
+        <h3>欢迎使用国内培训后台管理系统</h3>
+        <form class="m-t" role="form" method="post" id="frmxx" action="/Account/Login">
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="请输入LDAP账号用户名" required="" id="UserName" name="UserName"
+              value="">
+          </div>
+          <div class="form-group">
+            <input type="password" class="form-control" placeholder="请输入LDAP账号密码" required="" id="Password"
+              name="Password">
+          </div>
+          <button type="submit" class="btn btn-primary block full-width m-b">登 录</button>
+          <span class="text-danger field-validation-valid" data-valmsg-for="UserName" data-valmsg-replace="true"></span>
+          <span class="text-danger field-validation-valid" data-valmsg-for="Password" data-valmsg-replace="true"></span>
+          <span class="text-danger field-validation-valid" data-valmsg-for="VerifyCode" data-valmsg-replace="true"></span>
+      </form>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    </div>
+  <!-- <div class="footer">
+                    <a href="https://t-global-academy-admin.laiye.com">海外培训中心</a>
+                            </div> -->
+    <!-- <div v-for="item in items" :key="item.id" class="footer">
+                        <a v-for="item in items" href="item.url">{{ item.name }}</a>
+                      </div> -->
+    <div v-if="items.length > 0" class="footer">
+      <a v-for="item in items" :key="item.url" :href="item.url">{{ item.name }}</a>
+    </div>
+  </div>
 </template>
-
+<script>
+export default {
+  data() {
+    return {
+      items: []
+    };
+  },
+  created() {
+    // 发起POST请求
+    fetch('http://127.0.0.1:18080/api/backstageFriendLink', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ /* 请求体数据 */ })
+    })
+      .then(response => response.json())
+      .then(data => {
+        // 将返回的数据保存到组件的数据中
+        this.items = data;
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
+};
+</script>
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.footer {
+  height: 100px;
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+  font-size: 30px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import './assets/main.css'
+
+import './static/Content/HplusCSS/bootstrap.min.css?v=3.3.6'
+import './static/Content/HplusCSS/font-awesome.min.css?v=4.4.0'
+import './static/Content/HplusCSS/animate.min.css'
+import './static/Content/HplusCSS/style.css?v=4.1.0'
 
 import { createApp } from 'vue'
 import App from './App.vue'
